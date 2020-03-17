ba6b5dbfdf8356ae6d3ffbb5e19a51a2de2f9249
anoopa <anoopa.baby@expressbase.com>
2020-03-17 13:17:38 +0530

    Selenium - Anoopa

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace Selenium
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("test case started ");
+        //    IWebDriver driver = new ChromeDriver("D:\\Selenium\\Selenium\\bin\\Debug\\netcoreapp2.1");
+        //    driver.Navigate().GoToUrl("https://www.google.com/");
+        //    Thread.Sleep(2000);
+        //    IWebElement ele = driver.FindElement(By.Name("q"));
+        //    ele.SendKeys("expressbase");
+        //    Thread.Sleep(2000);
+        //    IWebElement ele1 = driver.FindElement(By.Name("btnK"));
+        //    ele1.Click();
+        //    Thread.Sleep(3000);
+        //    driver.Close();
+        //    Console.Write("test case ended ");
+        //}
+    }
+}
