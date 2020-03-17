a7df4eb92ee4c7ed6ceaeb0c268c59a35ac4ff59
anoopa <anoopa.baby@expressbase.com>
2020-03-17 13:37:35 +0530

    Selenium - test

---
+++ ObjectRepository/Login.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+
+namespace UITests.ObjectRepository
+{
+    public class Login
+    {
+        private IWebDriver driver;
+
+        public Login(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement UserName
+        {
+            get
+            {
+                return this.driver.FindElement(By.Name("uname"));
+            }
+        }
+
+        public IWebElement Password
+        {
+            get
+            {
+                return this.driver.FindElement(By.Name("pass"));
+            }
+        }
+
+        public IWebElement LoginButton
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("logNow"));
+            }
+        }
+
+        public IWebElement UNameCheckValidator
+        {
+            get
+            {
+                return this.driver.FindElement(By.XPath("//div[@validator='email']/div[2]"));
+            }
+        }
+
+        public IWebElement PasswordCheckValidator
+        {
+            get
+            {
+                return this.driver.FindElement(By.XPath("//div[@validator='password']/div[2]"));
+            }
+        }
+    }
+}
