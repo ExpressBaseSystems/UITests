1ffa237823f4e483f3644485f2125bb59220a622
anoopa <anoopa.baby@expressbase.com>
2020-03-25 13:14:24 +0530

    Testing - Tenant Signup

---
+++ ObjectRepository/Tenant/TenantSignUp.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace UITests.ObjectRepository.Tenant
+{
+    class TenantSignUp
+    {
+        private IWebDriver driver;
+
+        public TenantSignUp(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement SignUpButton
+        {
+            get
+            {
+                return this.driver.FindElement(By.ClassName("callActionBtn"));
+            }
+        }
+
+        public IWebElement Email
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("email"));
+            }
+        }
+
+        public IWebElement Password
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("inputPassword"));
+            }
+        }
+
+        public IWebElement Name
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("name"));
+            }
+        }
+
+        public IWebElement Country
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("country"));
+            }
+        }
+
+        public IWebElement JoinNow
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("save-profile"));
+            }
+        }
+
+        public IWebElement EmailError1
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("emaillbl"));
+            }
+        }
+        
+        public IWebElement EmailError2
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("emaillbl2"));
+            }
+        }
+
+        public IWebElement StrongPassword
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("psdinfo1"));
+            }
+        }
+
+        public IWebElement NameCheck
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("namelbl"));
+            }
+        }
+    }
+}
---
+++ TestCases/Tenant/TenantNewSolutionTestCase.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using UITests.DataDriven.Tenant;
+
+namespace UITests.TestCases.Tenant
+{
+    [TestFixture]
+    public class TenantNewSolutionTestCase : TenantNewSolution
+    {
+        private IWebDriver driver;
+        ObjectRepository.Tenant.TenantNewSolution l;
+        string url = "https://myaccount.eb-test.cloud/";
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            l = new ObjectRepository.Tenant.TenantNewSolution(driver);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+
+    }
+}
