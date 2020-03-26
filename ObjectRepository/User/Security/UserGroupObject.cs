d99192a49cadc4e17d5ad26c11f84d66d847f3b5
Kurian Cherian <kurianlive@LIVE.COM>
2020-03-26 21:06:09 +0530

    User Group Test Case

---
+++ ObjectRepository/User/Security/UserGroupObject.cs
@@ -0,0 +1,134 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using UITests.DataDriven;
+using UITests.ObjectRepository.User;
+
+namespace UITests.ObjectRepository.User.Security
+{
+    class UserGroupObject
+    {
+        private IWebDriver driver;
+
+        public UserGroupObject(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement locateUserGroupMenu
+        {
+            get
+            {
+                return this.driver.FindElement(By.XPath("//*[@id='appList']/div/ul/li/ul/li[3]/a"));
+            }
+            
+        }
+        public IWebElement locateUserGroupInnerMenu
+        {
+            get
+            {
+                return this.driver.FindElement(By.XPath("//*[@id='ebm-security']/div[2]/ul/li[3]/a"));
+            }
+
+        }
+        public IWebElement CreateUserGroup
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("btnNewCmnList"));
+            }
+
+        }
+
+        public IWebElement Name
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("txtUserGroupName"));
+            }
+        }
+        
+        public IWebElement Description
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("txtUserGroupDescription"));
+            }
+        }
+        public IWebElement AddUserButton
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("btnAddModalAdd_Users"));
+            }
+        }
+        public IWebElement SearchForUser
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("txtSearchAdd_Users"));
+            }
+        }
+        public IWebElement SelectUser
+        {
+            get
+            {
+                return this.driver.FindElement(By.XPath("//*[@id='divSearchResultsAdd_Users']/div[1]/div[1]/input"));
+            }
+        }
+        public IWebElement AddSelectedUser
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("btnModalOkAdd_Users"));
+            }
+        }
+        public IWebElement ConstrainTab 
+        {
+            get
+            {
+                return this.driver.FindElement(By.XPath("//*[@class='eb-usrgrp-pane h-100']/ul/li[2]/a"));
+            }
+        }
+        public IWebElement NewIP
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("btnAddModalNew_IP"));
+            }
+        }
+        public IWebElement IpAddress
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("txtIpAddressNew_IP"));
+            }
+        }
+        public IWebElement IpDescription
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("txtIpDescriptionNew_IP"));
+            }
+        }
+        public IWebElement SaveIp
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("btnModalOkNew_IP"));
+            }
+        }
+        public IWebElement SaveUserGroup
+        {
+            get
+            {
+                return this.driver.FindElement(By.Id("btnSaveAll"));
+            }
+        }
+    }
+}
---
+++ TestCases/User/Security/UserGroupTestCase.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UITests.DataDriven;
+using UITests.ObjectRepository.User;
+using UITests.ObjectRepository.User.Security;
+
+namespace UITests.TestCases.User.Security
+{
+    [TestFixture]
+    public class UserGroupTestCase
+    {
+        IWebDriver driver;
+        UserLogin ul;
+        UserGroupObject ug;
+        BrowserOps browserOps = new BrowserOps();
+
+        [SetUp]
+        public void Initialize()
+        {
+            if (driver == null)
+            {
+                browserOps.Init_Browser();
+                driver = browserOps.getDriver;
+                ul = new UserLogin(driver);
+                ug = new UserGroupObject(driver);
+            }
+        }
+
+        [Test, Order(1)]
+        public void UserLogin()
+        {
+            browserOps.Goto("https://hairocraft.eb-test.cloud/");
+            ul.UserName.SendKeys("hairocraft123@gmail.com");
+            ul.Password.SendKeys("12345678");
+            ul.LoginButton.Click();
+        }
+
+        [TestCaseSource("UserGroupObjects"), Order(2)]
+        public void CreateUserGroup(dynamic UserGroupObject)
+        {
+            browserOps.implicitWait(300);
+            ug.locateUserGroupMenu.Click();
+            browserOps.implicitWait(50);
+            ug.locateUserGroupInnerMenu.Click();
+            browserOps.implicitWait(50);
+            ug.CreateUserGroup.Click();
+            browserOps.implicitWait(50);
+
+
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+
+
+            browserOps.implicitWait(50);
+            ug.Name.SendKeys(UserGroupObject.Name);
+            ug.Description.SendKeys(UserGroupObject.Description);
+
+            browserOps.implicitWait(50);
+            ug.AddUserButton.Click();
+            ug.SearchForUser.SendKeys(UserGroupObject.UserName);
+            browserOps.implicitWait(100);
+            ug.SelectUser.Click();
+            browserOps.implicitWait(50);
+            ug.AddSelectedUser.Click();
+
+            browserOps.implicitWait(100);
+            ug.ConstrainTab.Click();
+            ug.NewIP.Click();
+            ug.IpAddress.SendKeys(UserGroupObject.Ip);
+            ug.IpDescription.SendKeys(UserGroupObject.IpDescription);
+            ug.SaveIp.Click();
+
+            ug.SaveUserGroup.Click();
+           
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            //driver.Close();
+        }
+
+        private static List<EbTestItem> UserGroupObjects()
+        {
+            return GetDataFromXML.GetDataFromFile(@"TestData\User\Security\UserGroup.xml");
+        }
+    }  
+}
